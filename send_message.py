08cf5aa9e2af8c729e9d91e3ae003927bf4bfa47
olegnovikov <o.novikov@ciat.net.ua>
2020-11-13 10:56:57 +0200

    added smtplib

--- send_message.py
+++ send_message.py
@@ -2,7 +2,11 @@ import sys
 import os
 import datetime
 
-from realty.settings import EMAIL_HOST_USER
+MAIL_SERVER = os.environ.get('MAIL_SERVER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+
 
 project_dir = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(project_dir)
@@ -27,12 +31,29 @@ if qs.exists():
         html_content += '<hr/><br/><br/>'
     qs.update(sent=True)
     subject = 'RealEstate'
-    from_email, to = EMAIL_HOST_USER, EMAIL_HOST_USER
-    text_content = 'This is an important message.'
+    # from_email, to = EMAIL_HOST_USER, EMAIL_HOST_USER
+    # text_content = 'This is an important message.'
 
-    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
-    msg.attach_alternative(html_content, "text/html")
-    msg.send()
+    # msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    # msg.attach_alternative(html_content, "text/html")
+    # msg.send()
+    import smtplib
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = EMAIL_USER
+    mail = smtplib.SMTP()
+    mail.connect(MAIL_SERVER, 25)
+    mail.ehlo()
+    mail.starttls()
+    mail.login(EMAIL_USER, EMAIL_PASSWORD)
+
+    part = MIMEText(html_content, 'html')
+    msg.attach(part)
+    mail.sendmail(EMAIL_USER, [EMAIL_HOST_USER], msg.as_string())
+    mail.quit()
 
 today = datetime.date.today()
 ten_days_ago = datetime.date.today() - datetime.timedelta(10)
