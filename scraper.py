b0d683e61ddcbe408752bf6c490b2b61fc9d938c
olegnovikov <o.novikov@ciat.net.ua>
2020-12-02 12:36:21 +0200

    Two rooms too

--- scraper.py
+++ scraper.py
@@ -32,48 +32,50 @@ headers = [
         'Mozilla/5.0 (Windows NT 6.1; rv:53.0) Gecko/20100101 Firefox/53.0',
     'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
     ]
-base_url = 'https://www.olx.ua/nedvizhimost/kvartiry-komnaty/prodazha-kvartir-komnat/kiev/?search%5Bfilter_float_number_of_rooms%3Afrom%5D=3&search%5Bfilter_float_number_of_rooms%3Ato%5D=3&search%5Bdistrict_id%5D=15&currency=USD'
+base_url = 'https://www.olx.ua/nedvizhimost/kvartiry-komnaty/prodazha-kvartir-komnat/kiev/?search%5Bfilter_float_number_of_rooms%3Afrom%5D={room}&search%5Bfilter_float_number_of_rooms%3Ato%5D={room}&search%5Bdistrict_id%5D=15&currency=USD'
+
 domain = 'https://www.olx.ua/'
 data = []
 urls = []
 # urls.append(base_url)
-req = session.get(base_url, headers=headers[randint(0, 2)])
 today = datetime.today()
 today_str = datetime.strftime(today, '%H:%M %d-%m-%Y')
-if req.status_code == 200:
-    bsObj = BS(req.text, "html.parser")
-    table = bsObj.find('table', attrs={'id': 'offers_table'})
-    tr_list = bsObj.find_all('tr', attrs={'class': 'wrap'})
-    for tr in tr_list:
-        td = tr.find('td', attrs={'class': 'offer'})
-        # if 'promoted' in td['class']:
-        #     continue
-        title_cell = td.find('td', attrs={'class': 'title-cell'})
-        title = title_cell.find('h3')
-        href = title.a['href']
-        price = "No price"
-        last_price = 0
-        td_price = td.find('td', attrs={'class': 'td-price'})
-        if td_price:
-            price = td_price.text.replace('\n', ' ').replace('\t', ' ')
-            last_price = int(''.join(c for c in price if c.isdigit()))
-        bottom_cell = td.find('td', attrs={'class': 'bottom-cell'})
-        added_time = None
-        f_text = bottom_cell.text.replace('\n', ' ').replace('\t', ' ')
-        _m = re.findall(TIME_PATTERN, f_text)
-        if _m:
-            _time = _m[0]
-            hour, minute = _time.split(":")
-            added_time = int(hour) * 60 + int(minute)
-        history_data = {today_str: price}
-        data.append({'url': href,
-                     'title': title.text,
-                     'price': price,
-                     'added_time': added_time,
-                     'last_price': last_price,
-                     'history_data': history_data
-                     })
-        urls.append(href)
+for room in [2, 3]:
+    req = session.get(base_url.format(room=room), headers=headers[randint(0, 2)])
+    if req.status_code == 200:
+        bsObj = BS(req.text, "html.parser")
+        table = bsObj.find('table', attrs={'id': 'offers_table'})
+        tr_list = bsObj.find_all('tr', attrs={'class': 'wrap'})
+        for tr in tr_list:
+            td = tr.find('td', attrs={'class': 'offer'})
+            # if 'promoted' in td['class']:
+            #     continue
+            title_cell = td.find('td', attrs={'class': 'title-cell'})
+            title = title_cell.find('h3')
+            href = title.a['href']
+            price = "No price"
+            last_price = 0
+            td_price = td.find('td', attrs={'class': 'td-price'})
+            if td_price:
+                price = td_price.text.replace('\n', ' ').replace('\t', ' ')
+                last_price = int(''.join(c for c in price if c.isdigit()))
+            bottom_cell = td.find('td', attrs={'class': 'bottom-cell'})
+            added_time = None
+            f_text = bottom_cell.text.replace('\n', ' ').replace('\t', ' ')
+            _m = re.findall(TIME_PATTERN, f_text)
+            if _m:
+                _time = _m[0]
+                hour, minute = _time.split(":")
+                added_time = int(hour) * 60 + int(minute)
+            history_data = {today_str: price}
+            data.append({'url': href,
+                         'title': title.text,
+                         'price': price,
+                         'added_time': added_time,
+                         'last_price': last_price,
+                         'history_data': history_data
+                         })
+            urls.append(href)
 qs = RealEstate.objects.filter(url__in=urls)
 used = {q.url: q for q in qs}
 for d in data:
